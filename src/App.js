617e6d0e3b3460303a9b64bd812b35a3d82af55d
kadnaoui <abderrahimkdn@gmail.com>
2022-06-30 20:54:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState, Suspense } from "react";
+import { Section } from "./components/section";
 
-function App() {
+import { Canvas , useFrame} from "@react-three/fiber";
+import { Html, useGLTF,OrbitControls } from "@react-three/drei";
+
+function Model({ url }) {
+  const gltf = useGLTF(url, true);
+  
+  return <primitive object={gltf.scene} dispose={null} />;
+  
+}
+
+const HTMLContent = () => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.x += 0.01));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Section factor={1.5} offset={1}>
+      <group position={[0, 218, 90]}>
+        <mesh ref={ref}  position={[0, 35, 0]}>
+    
+          <Model url='/scene.gltf'/>
+        </mesh>
+
+      </group>
+    </Section>
+  );
+};
+
+const Lights = () => {
+  return (
+    <>
+      {/* Ambient Light illuminates lights for all objects */}
+      <ambientLight intensity={0.3} />
+      {/* Diretion light */}
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+     
+  
+    </>
+  );
+};
+
+
+export default function App() {
+
+
+  return (
+    <div style={{height:"100vh"}}>
+      {/* R3F Canvas */}
+      <Canvas
+        colormanagement='true'
+        camera={{ position: [0, 0, 120], fov: 70 }}>
+          <Lights/>
+          <HTMLContent
+            bgColor='#f15946'
+            position={250}>
+            
+          </HTMLContent>
+         
+      </Canvas>
+     
     </div>
   );
 }
-
-export default App;
